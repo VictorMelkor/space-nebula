253e8d40adb7733e472e27c00408acf4b47ea7f2
Victor Rodrigues <vsr1988@hotmail.com>
2024-10-15 13:38:20 -0300

    refatorando

--- example/urls.py
+++ example/urls.py
@@ -1,10 +1,11 @@
 # example/urls.py
 from django.urls import path
 
-from example.views import index, imagem
+from example.views import index, imagem, buscar
 
 
 urlpatterns = [
     path('', index, name='index'),
-    path('imagem/<int:foto_id>', imagem, name='imagem')
+    path('imagem/<int:foto_id>', imagem, name='imagem'),
+    path('buscar', buscar, name='buscar')
 ]
--- example/views.py
+++ example/views.py
@@ -11,3 +11,13 @@ def index(request):
 def imagem(request, foto_id):
     fotografia = get_object_or_404(Fotografia, pk=foto_id)
     return render(request, 'imagem.html', {"fotografia": fotografia})
+
+def buscar(request):
+    fotografias = Fotografia.objects.order_by('data_fotografia').filter(publicada=True)
+
+    if "buscar" in request.GET:
+        nome_a_buscar = request.GET['buscar']
+        if nome_a_buscar:
+            fotografias = fotografias.filter(nome__icontains=nome_a_buscar)
+
+    return render(request, 'buscar.html', {"cards": fotografias})
